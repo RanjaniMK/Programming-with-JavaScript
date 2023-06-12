fd0eacb101b9e43f01d46238b6cd5d0f19124350
Ranjani M K <ranjanimk3@gmail.com>
2023-06-12 20:24:31 +0530

    Adding files

---
+++ variable.js
@@ -0,0 +1,12 @@
+var petDog = "Rex"
+var petCat="Pepper"
+console.log(petDog)
+console.log(petCat)
+console.log("My dog's name is", petDog)
+console.log("My cat's name is ", petCat)
+var catSound = "Purr"
+var dogSound = "Woof"
+console.log(petDog,"says",dogSound)
+console.log(petCat,"says", catSound)
+catSound="Meow"
+console.log(petCat, "now says", catSound)
