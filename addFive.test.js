6f38fc5616936377abb89d5a1e3349fff1aecdc7
Ranjani M K <ranjanimk3@gmail.com>
2023-06-14 11:42:54 +0530

    test successful

--- addFive.test.js
+++ addFive.test.js
@@ -1,6 +1,6 @@
-const {default:TestRunner}= require("jest-runner");
+
 const addFive = require('./addFive');
-TestRunner('returns the number plus 5', ()=>{
+test('returns the number plus 5', ()=>{
     expect(addFive(1)).toBe(6);
 
 })
