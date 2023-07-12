d8b7eaa27bed39d4d07b44965123b3f3d2f1fc3b
Ranjani M K <ranjanimk3@gmail.com>
2023-07-12 21:01:04 +0530

    adding files

---
+++ DestructureArrayObjects.js
@@ -0,0 +1,17 @@
+let {PI}= Math;
+PI;
+
+
+
+let {pi}=Math;
+pi;
+
+
+
+PI ===Math.PI;
+
+
+PI=1;
+
+
+PI===Math.PI;
---
+++ ObjectEntries.js
@@ -0,0 +1,8 @@
+const car4 ={
+
+    speed:400,
+    color:'magenta'
+
+
+}
+console.log(Object.entries(car4));
