8e3392fb8a90ac01b13db2d35930679eb73d6bd2
Ranjani M K <ranjanimk3@gmail.com>
2023-07-19 13:43:44 +0530

    adding files

---
+++ objectKeysUsageAgain.js
@@ -0,0 +1,9 @@
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  };
+  const keys = Object.keys(person);
+
+  console.log(keys) // O/T: (4) ['firstName', 'lastName', 'age', 'eyeColor']
