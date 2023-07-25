07d59c473c53d9818a5ca40e6a870cdca44bb4ee
Ranjani M K <ranjanimk3@gmail.com>
2023-07-25 21:08:41 +0530

    adding files

---
+++ addingMemberstoArray.js
@@ -0,0 +1,8 @@
+//adding new members to array without using push()
+//adding new members to an existing array using spread operator
+
+let veggies = ['onion', 'parsley'];
+veggies = [...veggies, 'carrot', 'beetroot'];
+console.log(veggies);
+
+
